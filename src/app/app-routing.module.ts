71ad092991c882c16ad375885c1a4766a930ca0f
eloisa <eloisantunesp@gmail.com>
2021-11-18 18:46:59 -0300

    config-menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './views/header/header.component';
+import { MenuComponent } from './views/menu/menu.component';
 import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
   {
   path: '',
   component:  HeaderComponent
+  },
+  {
+  path: '',
+  component:  MenuComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon'; 
 import { HeaderComponent } from './views/header/header.component';
+import { MenuComponent } from './views/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    MatMenuModule,
     MatIconModule
   ],
   providers: [],
